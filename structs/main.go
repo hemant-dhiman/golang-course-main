b10877be57ded9c69842caf83020f8e40685dde2
Hemant Dhiman <hemant.dhiman92@gmail.com>
2021-09-30 17:29:03 +0530

    nested structs and initialization of it.

--- structs/main.go
+++ structs/main.go
@@ -5,13 +5,36 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	emain   string
+	zipCode int
 }
 
 func main() {
-	//alex := person{"Alex", "Anderson"}
+	//alex := person{"Alex", "Anderson"} // 1 way of declaring struct
 	//or
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	//alex := person{firstName: "Alex", lastName: "Anderson"} // 2nd way of declaring struct
+	//fmt.Println(alex)
 
-	fmt.Println(alex)
+	// var alex person // 3rd way of declaring struct
+
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			emain:   "email.com",
+			zipCode: 123456,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
